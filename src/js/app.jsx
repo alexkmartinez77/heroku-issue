5f082b745866bf3b56749dde7ccedf2b11dbdd76
Alex Martinez <alexkmartinez77@gmail.com>
2021-10-07 21:16:53 -0700

    onClick passes state object to calcualte funtion which deconstructs, calculates payment and displays payment message.

--- src/js/app.jsx
+++ src/js/app.jsx
@@ -11,6 +11,7 @@ export default class App extends React.Component {
     this.updateBalance = this.updateBalance.bind(this);
     this.updateRate = this.updateRate.bind(this);
     this.updateTerm = this.updateTerm.bind(this);
+    this.calculate  = this.calculate.bind(this);
   }
 
   updateBalance(event){
@@ -31,20 +32,24 @@ export default class App extends React.Component {
     });
   }
 
-  calculate(){
-
+  calculate(stateObject){
+    console.log(stateObject);
+    const {balance, rate, term} = stateObject;
+    document.getElementById("output").innerText = "$1945.09 is your payment";
   }
   
   render() {
     return (
       <div className='container'>
-        <input name="balance" type="number" value={this.state.balance} onChange={this.updateBalance}/>
-        <input name="rate" type="number" step=".01" value={this.state.rate} onChange={this.updateRate}/>
-        <select name="term" onChange={this.updateTerm}>
-          <option value="15">15</option>
-          <option value="30">30</option>
-        </select>
-        <button name="submit">CALCULATE</button>
+        <div><input name="balance" type="number" value={this.state.balance} onChange={this.updateBalance}/></div>
+        <div><input name="rate" type="number" step=".01" value={this.state.rate} onChange={this.updateRate}/></div>
+        <div>
+          <select name="term" onChange={this.updateTerm}>
+            <option value="15">15</option>
+            <option value="30">30</option>
+          </select>
+        </div>
+        <div><button name="submit" onClick={() => this.calculate(this.state)}>CALCULATE</button></div>
         <div name="output" id="output"></div>
       </div>
     );
