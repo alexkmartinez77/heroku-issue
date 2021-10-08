768517b9c48d88dcbf3fb15f91e3c21d59d3afc7
Alex Martinez <alexkmartinez77@gmail.com>
2021-10-07 20:28:26 -0700

    Adds and binds 2 functions that update balance and rate respectively as the user enters that data in the input.

--- src/js/app.jsx
+++ src/js/app.jsx
@@ -7,20 +7,37 @@ export default class App extends React.Component {
       balance: 100000,
       rate: 5,
       term: 15,
-
     }
+    this.updateBalance = this.updateBalance.bind(this);
+    this.updateRate = this.updateRate.bind(this);
+  }
+
+  updateBalance(event){
+    this.setState({
+      balance: event.target.value,
+    });
+  }
+
+  updateRate(event){
+    this.setState({
+      rate: event.target.value,
+    });
+  }
+
+  calculate(){
+
   }
   
   render() {
     return (
       <div className='container'>
-        <input name="balance" type="number" value="this.state.balance"></input>
-        <input name="rate" type="number" step=".01" value="this.state.balance"></input>
+        <input name="balance" type="number" value={this.state.balance} onChange={this.updateBalance}/>
+        <input name="rate" type="number" step=".01" value={this.state.rate} onChange={this.updateRate}/>
         <select name="term">
           <option value="15">15</option>
           <option value="30">30</option>
         </select>
-        <button name="submit"></button>
+        <button name="submit">CALCULATE</button>
         <div name="output" id="output"></div>
       </div>
     );
