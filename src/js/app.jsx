5916e52b80b9a50dfcfc0caaa385c35845744352
Alex Martinez <alexkmartinez77@gmail.com>
2021-10-07 18:32:19 -0700

    Adds a constructor initiating balance, rate and term in state.

--- src/js/app.jsx
+++ src/js/app.jsx
@@ -1,12 +1,21 @@
 import React from 'react';
 
 export default class App extends React.Component {
-  // your Javascript goes here
+  constructor(props){
+    super(props);
+    this.state = {
+      balance: 100000,
+      rate: 5,
+      term: 15,
+
+    }
+  }
+  
   render() {
     return (
       <div className='container'>
-        <input name="balance" type="number"></input>
-        <input name="rate" type="number" step=".01"></input>
+        <input name="balance" type="number" value="this.state.balance"></input>
+        <input name="rate" type="number" step=".01" value="this.state.balance"></input>
         <select name="term">
           <option value="15">15</option>
           <option value="30">30</option>
