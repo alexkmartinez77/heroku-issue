0ee6d43b967ffc9c33307413666196d04a03b5ad
Alex Martinez <alexkmartinez77@gmail.com>
2021-10-07 18:16:10 -0700

    Adds the JSX elements in the return.

--- src/js/app.jsx
+++ src/js/app.jsx
@@ -5,7 +5,14 @@ export default class App extends React.Component {
   render() {
     return (
       <div className='container'>
-        {/* your JSX goes here */}
+        <input name="balance" type="number"></input>
+        <input name="rate" type="number" step=".01"></input>
+        <select name="term">
+          <option value="15">15</option>
+          <option value="30">30</option>
+        </select>
+        <button name="submit"></button>
+        <div name="output" id="output"></div>
       </div>
     );
   }
